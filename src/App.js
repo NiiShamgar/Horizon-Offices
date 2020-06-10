c9d3e482a19486721f5dddc16af31558e2ea8829
Nii Shamgar <niishamgar@gmail.com>
2020-06-10 11:01:37 +0000

    Horizon done

--- src/App.js
+++ src/App.js
@@ -2,24 +2,122 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+   
+    
+    <div>
+      <div style={{flex:1, display:'flex', flexDirection:'row', paddingTop: '10px', alignItems: 'right'}}>
+        <img src="images/logo.jpg" alt="Horizon Logo" style={{height:'50px', width:'200px', marginLeft:'30px'}}/>
+        <img src="images/ghana.jpg" alt="Ghana" style={{height:'20px', width: '30px',marginLeft: '400px',}}/>
+        <img src="images/nigeria.jpeg" alt="Nigeria" style={{height:'20px', width: '30px', marginLeft:'10px'}}/>
+        <img src="images/kenya.jpg" alt="Kenya" style={{height:'20px', width: '30px', marginLeft:'10px'}}/>
+        <a href="#" style={{color: 'grey', marginLeft: '500px'}}> About Us</a>
+          </div>
+              <div>
+                <img src="images/silverstar2.jpg" alt="aro1" style={{height:'90vh', width: '100vw'}}/>  
+                  
+
+                  <div>
+                    <h3 style={{marginLeft: '630px'}}>PLANS</h3>
+
+                        <div>
+                          <span style={{flex:1, display:'flex', flexDirection:'row',paddingTop:'10px',marginBottom:'70px'}}>
+                            <div style={{ marginRight: '40px', width:'20vw', height: '20vh', marginLeft: '60px'}}>
+                              <img src="images/office1.jpg" alt="Office" style={{height: '25vh', width: '20vw'}}/>
+                                <h3 style={{marginLeft: '50px'}}>VIRTUAL OFFICES</h3>
+                                  <p >Our virtual offices allow your business to get our location address,
+                                    phone services and also you become a member of our community with
+                                    benefits such as access to our meeting rooms.</p>
+                            </div>
+
+                            <div style={{ width:'20vw', height: '20vh', }}>
+                              <img src="images/office2.jpg" alt="Office" style={{height: '25vh', width: '20vw'}}/>
+                                <h3 style={{marginLeft: '50px'}}>CUSTOMIZED OFFICES</h3>
+                                    <p>Our customized offices offer a great location for your business
+                                    and you can customize your office to suit your preference.
+                                    Also, you become a member of our community with
+                                    benefits such as access to our meeting rooms.</p>
+                            </div> 
+
+                            <div style={{ marginLeft: '40px', width:'20vw', height: '20vh'}}>
+                              <img src="images/office3.jpg" alt="Office" style={{height: '25vh', width: '20vw'}}/>
+                                  <h3 style={{marginLeft: '50px'}}>SERVICED OFFICES</h3>
+                                        <p>Our serviced offices offer a great location for your business
+                                            and we provide services such as internet, utilities and offer
+                                            from our partners. Also you become a member of our community with
+                                            benefits such as access to our meeting rooms.
+                                        </p>
+                            </div>
+
+                            <div style={{marginLeft: '40px', marginBottom: '20px', width:'20vw', height: '20vh'}}>
+                              <img src="images/office4.jpg" alt="Office" style={{height: '25vh', width: '20vw'}}/>
+                                <h3 style={{marginLeft: '50px'}}>MEETING ROOMS</h3>
+                                        <p>Our meeting rooms offer a great location for our meeting with
+                                            your clients or business partners. We provide presentation
+                                            equipment and coffee. Also you become a member of our community
+                                            with benefits such as access to our meeting rooms. 
+                                      </p>
+                            </div>        
+                        </span>
+                        </div>
+                        </div>
+                        </div>
+                       
+                        
+                       
+
+
+                        <div>
+                         
+                            <span style={{flex:1, display:'flex', flexDirection:'row',paddingTop:'10px',marginTop: '250px',height:'50vh', width:'100vw', backgroundColor: 'blue'}}>
+                              <div  style={{ marginLeft: '100px', height:'50vh', width:'25vw'}}>
+                               <h3 style={{color: 'orange', paddingLeft: '60px'}}>VISIT US IN KENYA</h3>
+                                <h4 style={{color: 'yellowgreen', paddingLeft: '90px'}}>Nairobi, Kenya</h4>
+                                 <p style={{color:'white'}}>Belgravia Centre, 14 Riverside drive,
+                                  4th floor, Off Riverside Drive,
+                                  Hanover Centre, 14 Riverside Drive
+                                  6th floor, Off Riverside Drive
+                                </p>
+                                </div>
+
+
+                                <div  style={{ marginLeft: '100px', height:'50vh', width:'25vw'}}>
+                                  <h3 style={{color: 'orange',paddingLeft: '60px'}}>VISIT US IN GHANA</h3>
+                                    <h4 style={{color: 'yellowgreen',paddingLeft: '100px'}}>Accra, Ghana</h4>
+                                        <p style={{color:'white',paddingLeft: '10px'}}>One Airport Square, Airport City
+                                           8th Floor</p>
+                                </div>
+
+
+                                <div  style={{ marginLeft: '100px', height:'40vh', width:'25vw'}}>
+                                  <h3 style={{color: 'orange', paddingLeft: '50px'}}>VISIT US IN NIGERIA</h3>
+                                    <h4 style={{color: 'yellowgreen',paddingLeft: '80px'}}>Lagos, Nigeria</h4>
+                                        <p style={{color:'white',paddingLeft: '60px'}}>Sterling Bank Building
+                                        </p>
+                            
+                            </div>
+                            </span>
+                          
+                          
+                          
+                        
+                          
+                         
+                    
+                          
+                
+
+           
+
+         </div>
     </div>
+            
+  
+
+      
   );
 }
 
